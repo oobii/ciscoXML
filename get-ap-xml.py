5c605172f65632c069255e8109b93ee9fe243a01
martynov <vvayfarer@yandex.com>
2017-12-01 17:57:19 -0500

    extract a collection of elements by their tag name

--- get-ap-xml.py
+++ get-ap-xml.py
@@ -14,13 +14,10 @@ responseString = response.read().decode("utf-8")
 dom = xml.dom.minidom.parseString(responseString)
 xml = dom.toprettyxml()
 print(xml)
-floor_element = dom.firstChild
-if floor_element.hasChildNodes :
-    child = floor_element.firstChild
-    while child is not None:
-        if child.tagName == 'AccessPoint' :
-            output = child.tagName + ": " + child.getAttribute('name') + '\t eth: ' + child.getAttribute('ethMacAddress')
-            print(output)
-        child = child.nextSibling
-        
+access_points = dom.getElementsByTagName('AccessPoint')
+for access_point in access_points:
+    ap_name = access_point.getAttribute('name')
+    ap_eth_addr = access_point.getAttribute('ethMacAddress')
+    ap_ip_addr = access_point.getAttribute('ipAddress')
+    print(access_point.tagName + ": " + ap_name + '\t eth: ' + ap_eth_addr + '\t ip: ' + ap_ip_addr)
 response.close()
